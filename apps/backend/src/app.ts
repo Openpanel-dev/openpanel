2a99eee71c13620ed2fcee146fdf3497771eae9b
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2023-10-12 07:24:21 +0200

    update docker and change api path

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -4,18 +4,15 @@ import profiles from './routes/profiles'
 import { authMiddleware } from "./middlewares/auth";
 import morgan from 'morgan'
 
-
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(morgan('tiny'))
 app.use(express.json());
-app.use(express.json());
+app.use(morgan(':method :url :status :response-time ms'))
+app.get("/", (req, res) => res.json("Welcome to Mixan"));
 app.use(authMiddleware)
-app.use(events)
-app.use(profiles)
-app.get("/ping", (req, res) => res.json("pong"));
-
+app.use('/api/sdk',events)
+app.use('/api/sdk',profiles)
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
