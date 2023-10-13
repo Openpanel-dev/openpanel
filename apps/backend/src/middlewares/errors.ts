b9fe6127fffaf9a4fbbf62222a7ca3fc038ae3e7
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2023-10-13 15:42:11 +0200

    log errors

--- apps/backend/src/middlewares/errors.ts
+++ apps/backend/src/middlewares/errors.ts
@@ -4,9 +4,14 @@ import { MixanErrorResponse } from "@mixan/types";
 
 export const errorHandler = (error: HttpError | Error, req: Request, res: Response, next: NextFunction) => {
   if(error instanceof HttpError) {
+    console.log('[HttpError]', error.toJson())
     return res.status(error.status).json(error.toJson())
   }
 
+  console.log('[UnknownError]', error.name, error.message)
+  if(error.stack) {
+    console.log(error.stack)
+  }
   return res.status(500).json({
     code: 500,
     status: 'error',
