753df4829603b1eb9cdc7089c71ce08215568b44
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-02-28 22:32:52 +0100

    fast fix for duration issue

--- apps/worker/src/jobs/events.ts
+++ apps/worker/src/jobs/events.ts
@@ -11,6 +11,9 @@ import { createSessionEnd } from './events.create-session-end';
 export async function eventsJob(job: Job<EventsQueuePayload>) {
   switch (job.data.type) {
     case 'createEvent': {
+      if (job.attemptsStarted > 1 && job.data.payload.duration < 0) {
+        job.data.payload.duration = 0;
+      }
       return await createEvent(job.data.payload);
     }
   }
