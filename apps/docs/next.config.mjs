1ca95442b91117942635bdf8b11373e56a212582
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-03-11 13:05:28 +0100

    add documentation

---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,33 @@
+import nextra from 'nextra';
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  transpilePackages: ['@mixan/queue'],
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  experimental: {
+    // Avoid "Critical dependency: the request of a dependency is an expression"
+    serverComponentsExternalPackages: ['bullmq'],
+  },
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+};
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+  flexsearch: {
+    codeblocks: false,
+  },
+  defaultShowCopyCode: true,
+});
+
+export default withNextra(config);
