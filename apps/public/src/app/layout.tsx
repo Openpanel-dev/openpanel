04929cd89e22b55a3eb9aaf7b03f1dc611cdeed5
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-03-14 07:10:31 +0100

    add openpanel/nextjs to public

--- apps/public/src/app/layout.tsx
+++ apps/public/src/app/layout.tsx
@@ -1,13 +1,13 @@
 import { cn } from '@/utils/cn';
-
-import '@/styles/globals.css';
-
 import type { Metadata } from 'next';
 import { Bricolage_Grotesque } from 'next/font/google';
-import Script from 'next/script';
+
+import { OpenpanelProvider } from '@openpanel/nextjs';
 
 import Footer from './footer';
 import { defaultMeta } from './meta';
+
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
   ...defaultMeta,
@@ -38,7 +38,12 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
-      {/* 301c6dc1-424c-4bc3-9886-a8beab09b615 */}
+      <OpenpanelProvider
+        clientId="301c6dc1-424c-4bc3-9886-a8beab09b615"
+        trackAttributes
+        trackScreenViews
+        trackOutgoingLinks
+      />
     </html>
   );
 }
