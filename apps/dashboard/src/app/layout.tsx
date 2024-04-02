8c42b9691ebf6032ff6f9499630b25f282d4af5e
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-04-02 11:37:26 +0200

    suppressHydrationWarning 🫣

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn('grainy min-h-screen bg-slate-100 font-sans antialiased')}
       >
