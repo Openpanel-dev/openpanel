5087166814a9df6a3be5b322bbd727d1057ff653
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-04-16 12:07:59 +0200

    try force dynamic

--- apps/dashboard/src/app/(auth)/sso-callback/page.tsx
+++ apps/dashboard/src/app/(auth)/sso-callback/page.tsx
@@ -1,5 +1,7 @@
 import { AuthenticateWithRedirectCallback } from '@clerk/nextjs';
 
+export const dynamic = 'force-dynamic';
+
 const SSOCallback = () => {
   return <AuthenticateWithRedirectCallback />;
 };
