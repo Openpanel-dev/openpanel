cc0f6a83f1e730611af9c80f41197e80a2b64bb6
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-04-17 09:49:18 +0200

    prisma

--- apps/dashboard/next.config.mjs
+++ apps/dashboard/next.config.mjs
@@ -1,3 +1,6 @@
+// @ts-expect-error
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -6,6 +9,13 @@ await import('./src/env.mjs');
 
 /** @type {import("next").NextConfig} */
 const config = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   reactStrictMode: true,
   transpilePackages: [
     '@openpanel/queue',
