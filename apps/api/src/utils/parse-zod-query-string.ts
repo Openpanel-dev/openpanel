1dcd501b13fb222984aa43f568c713190a030d43
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-06-12 22:13:54 +0200

    improve parseQueryString function

--- apps/api/src/utils/parse-zod-query-string.ts
+++ apps/api/src/utils/parse-zod-query-string.ts
@@ -4,7 +4,8 @@ export const parseQueryString = (obj: Record<string, any>): any => {
   return Object.fromEntries(
     Object.entries(obj).map(([k, v]) => {
       if (typeof v === 'object') return [k, parseQueryString(v)];
-      if (!isNaN(parseFloat(v))) return [k, parseFloat(v)];
+      if (/^-?[0-9]+(\.[0-9]+)?$/i.test(v) && !isNaN(parseFloat(v)))
+        return [k, parseFloat(v)];
       if (v === 'true') return [k, true];
       if (v === 'false') return [k, false];
       if (typeof v === 'string') {
