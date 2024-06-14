a7bed508b5041b48c24cdc520ecddeab736564d0
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-06-14 23:29:57 +0200

    #32 - fix search index docs

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -16,8 +16,8 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
   },
 };
 
