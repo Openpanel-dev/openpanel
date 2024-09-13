4bafa1641929fc502cec6fcf16405e5015556e51
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-09-13 10:21:22 +0200

    chore(logger): remove baselime

--- packages/logger/index.ts
+++ packages/logger/index.ts
@@ -4,12 +4,7 @@ import pino from 'pino';
 export function createLogger({ dataset }: { dataset: string }) {
   const targets: TransportTargetOptions[] =
     process.env.NODE_ENV === 'production' && process.env.BASELIME_API_KEY
-      ? [
-          {
-            target: '@baselime/pino-transport',
-            options: { baselimeApiKey: process.env.BASELIME_API_KEY, dataset },
-          },
-        ]
+      ? []
       : [
           {
             target: 'pino-pretty',
