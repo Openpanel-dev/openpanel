e8906e3d2ebee05e6aba931be2c412b5aaebbbc3
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-09-13 10:21:22 +0200

    chore(logger): remove baselime

--- apps/dashboard/src/instrumentation.ts
+++ apps/dashboard/src/instrumentation.ts
@@ -5,20 +5,5 @@ export async function register() {
     process.env.NODE_ENV === 'production' &&
     process.env.NEXT_RUNTIME === 'nodejs'
   ) {
-    const { BaselimeSDK, VercelPlugin, BetterHttpInstrumentation } =
-      // @ts-expect-error
-      await import('@baselime/node-opentelemetry');
-
-    const sdk = new BaselimeSDK({
-      serverless: true,
-      service: 'vercel-coderaxs-projects-d46dc62b',
-      instrumentations: [
-        new BetterHttpInstrumentation({
-          plugins: [new VercelPlugin()],
-        }),
-      ],
-    });
-
-    sdk.start();
   }
 }
