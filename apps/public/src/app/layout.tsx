dfe36feb70e2ccb93f64b23ff02329be18dad7fd
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-10-29 21:08:21 +0100

    fix(public): add white background (dark mode on mobile safari broken)

--- apps/public/src/app/layout.tsx
+++ apps/public/src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="light">
+    <html lang="en" className="light bg-white">
       <body
         className={cn(
           'grainy min-h-screen font-sans text-slate-900 antialiased',
