eb05e0ee333928c8237f1a3bf102f72f7887364b
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-11-24 22:01:53 +0100

    fix(dashboard): do not listen to notifications if projectid is missing

--- apps/dashboard/src/components/notifications/notification-provider.tsx
+++ apps/dashboard/src/components/notifications/notification-provider.tsx
@@ -6,6 +6,15 @@ import { toast } from 'sonner';
 
 export function NotificationProvider() {
   const { projectId } = useAppParams();
+
+  if (!projectId) return null;
+
+  return <InnerNotificationProvider projectId={projectId} />;
+}
+
+export function InnerNotificationProvider({
+  projectId,
+}: { projectId: string }) {
   useWS<Notification>(`/live/notifications/${projectId}`, (notification) => {
     toast(notification.title, {
       description: notification.message,
