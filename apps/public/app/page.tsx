54d1f9b6d6d355f2a9512bcac367e874bed19cf1
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2025-01-05 23:10:01 +0100

    chore(public): update home title

--- apps/public/app/page.tsx
+++ apps/public/app/page.tsx
@@ -11,7 +11,7 @@ import type { Metadata } from 'next';
 import { Suspense } from 'react';
 
 export const metadata: Metadata = {
-  title: 'An open-source alternative to Mixpanel',
+  title: 'OpenPanel | An open-source alternative to Mixpanel',
 };
 
 export const experimental_ppr = true;
