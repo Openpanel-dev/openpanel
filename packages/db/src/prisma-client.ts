38a93c0b2f91b884a7219a6a452dc419d530135b
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2025-01-30 22:23:41 +0000

    improve(db): add read replica

--- packages/db/src/prisma-client.ts
+++ packages/db/src/prisma-client.ts
@@ -1,15 +1,22 @@
 import { PrismaClient } from '@prisma/client';
+import { readReplicas } from '@prisma/extension-read-replicas';
 
 export * from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const getPrismaClient = () => {
+  return new PrismaClient({
+    log: ['error'],
+  }).$extends(
+    readReplicas({
+      url: process.env.DATABASE_URL_REPLICA ?? process.env.DATABASE_URL!,
+    }),
+  );
 };
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['error'],
-  });
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof getPrismaClient> | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? getPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
