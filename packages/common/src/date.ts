553e4cf675e24794358089df320e447362f9175c
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2026-03-02 13:18:34 +0100

    fix: ts issues

--- packages/common/src/date.ts
+++ packages/common/src/date.ts
@@ -1,6 +1,5 @@
+// biome-ignore lint/performance/noBarrelFile: lazy
 export { DateTime } from 'luxon';
-
-export type { DateTime };
 
 export function getTime(date: string | number | Date) {
   return new Date(date).getTime();
