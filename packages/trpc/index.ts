ac3a27175311e0c44894a2935ce05df610d1a77f
tyler <tylersmith1791@gmail.com>
2026-05-22 10:30:40 -0600

    add exports

--- packages/trpc/index.ts
+++ packages/trpc/index.ts
@@ -1,3 +1,5 @@
 export * from './src/root';
 export * from './src/trpc';
 export { getProjectAccess } from './src/access';
+export { getIsRegistrationAllowed } from './src/routers/auth';
+export { TRPCAccessError } from './src/errors'
